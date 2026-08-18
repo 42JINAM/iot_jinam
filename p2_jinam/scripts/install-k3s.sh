9e4bfba7efde548548e0602bd6ad3078c22a6e8a
42JINAM <jinam@student.42.fr>
2026-08-18 15:46:14 +0200

    update script

--- p2_jinam/scripts/install-k3s.sh
+++ p2_jinam/scripts/install-k3s.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
+set -eu
+
+sudo timedatectl set-ntp true 
+
+sudo apt-get update
+sudo apt-get install -y chrony
+
+sudo systemctl enable --now chrony
+sudo chronyc -a makestep
 
 curl -sfL https://get.k3s.io | sh -
+
+until sudo k3s kubectl get nodes >/dev/null 2>&1; do
+  echo "Waiting for Kubernetes API..."
+  sleep 2
+done
 
 sudo k3s kubectl wait \
   --for=condition=Ready \
@@ -19,6 +33,9 @@ sudo mkdir -p /opt/p2/manifests
 sudo cp -r /vagrant/manifests/. /opt/p2/manifests/
 
 sudo k3s kubectl apply -f /opt/p2/manifests
+sudo k3s kubectl rollout status deployment/app1 --timeout=120s
+sudo k3s kubectl rollout status deployment/app2 --timeout=120s
+sudo k3s kubectl rollout status deployment/app3 --timeout=120s
 
 sudo k3s kubectl get nodes
 sudo k3s kubectl get deployments
